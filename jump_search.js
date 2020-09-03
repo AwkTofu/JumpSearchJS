d7376ed4741d306079ef08d3d36567dd1a03ac69
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-09-03 16:22:41 -0400

    setting up the function def

--- jump_search.js
+++ jump_search.js
@@ -1,1 +1,10 @@
-console.log("test")
+let array = [ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610 ]; 
+
+function jumpSearch(arr, target) {
+	let step = Math.floor(Math.sqrt(arr.length));
+
+	
+	
+}
+
+console.log(jumpSearch(array, 8));
